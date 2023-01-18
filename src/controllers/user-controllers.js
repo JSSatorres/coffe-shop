3581eb40ecc3d0f881c93e966e9651465d30266a
jssatorres <juansataz.dev@gmail.com>
2023-01-18 11:17:35 +0100

    Feat (user) mvc

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -1,66 +1,79 @@
-const User = require ('../models/user-model')
-const bcryptjs = require ('bcryptjs');
+const User = require("../models/user-model");
+const bcryptjs = require("bcryptjs");
+/* const { validateEmail } = require('../helpers/db-validators'); */
 /* const inputValidator = require ("../middlewares") */
 
-const getUser = (req,res ) =>{
+const getUser = async (req, res) => {
+  const { limit = 5, since = 0 } = req.query;
 
-    const {q,name, apiKey,page=1, limit} = req.query;
+  // lanza una promesa y luega la otra con la forma del promise.all lanza todas las promesas a la vez
 
-    res.json({
-        msg:"get Api",
-        q,
-        name,
-        apiKey,
-        page,
-        limit
-    })
-
-}
-const addUser = async (req,res ) =>{
-
-    
-    const {name, email,password,rol} = req.body;
-    const user = new User({name, email,password,rol});
-
-    //authenticate unique email
-    const emailIs = await User.findOne({email})
-    if(emailIs){
-        return res.status(400).json({msg:'el correo ya esta registrado'})
-    }
-
-    //encrypt password
-    const salt = bcryptjs.genSaltSync()
-    user.password = bcryptjs.hashSync(password,salt)
-
-    //safe user in BD
-    await user.save()
+  /* const total = await  User.countDocuments({state :true}),
+    const users = await User.find({state :true})
+        .limit(Number(limit))
+        .skip(Number(since))
 
     res.json({
-        msg:"post Api",
-        user
-    })
-}
-const deleteUser = (req,res ) =>{
+        total,
+        users
+    }) */
 
-    const {id} = req.params;
+  const [total, users] = await Promise.all([
+    User.countDocuments({ state: true }),
+    User.find({ state: true }).limit(Number(limit)).skip(Number(since)),
+  ]);
 
-    res.json({
-        msg:"delete Api",
-        id
-    })
+  res.json({
+    total,
+    users,
+  });
+};
 
-}
-const editUser = (req,res) =>{
+const addUser = async (req, res) => {
+  const { name, email, password, rol } = req.body;
+  const user = new User({ name, email, password, rol });
 
-    const {name, age} = req.body;
-    const {id} = req.params;
+  //authenticate unique email
 
-    res.json({
-        msg:"put Api",
-        id
-    })
+  //encrypt password
 
-}
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
 
+  //safe user in BD
+  await user.save();
 
-module.exports={getUser,addUser,deleteUser,editUser}
+  res.json({
+    msg: "post Api",
+    user,
+  });
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  //delete user old method now you never delete somethig
+  /*const user = await User.findByIdAndDelete(id) */
+  const user = await User.findByIdAndUpdate(id, { state: false });
+
+  res.json({
+    id,
+  });
+};
+
+const editUser = async (req, res) => {
+  const { id } = req.params;
+  const { _id, google, email, ...rest } = req.body;
+
+  if (password) {
+    const salt = bcryptjs.genSaltSync();
+    rest.password = bcryptjs.hashSync(password, salt);
+  }
+  const user = await User.findByIdAndUpdate(id, rest);
+  res.json({
+    msg: "put Api",
+    id,
+  });
+};
+
+module.exports = { getUser, addUser, deleteUser, editUser };
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,31 +1,55 @@
-const {Router} = require('express');
-const { check } = require('express-validator');
-const {getUser,addUser,deleteUser,editUser}= require('../controllers/user-controllers');
-const { inputValidator } = require('../middlewares/inputValidate');
-const Role = require ('../models/user-model')
-
+const { Router } = require("express");
+const { check } = require("express-validator");
+const {
+  getUser,
+  addUser,
+  deleteUser,
+  editUser,
+} = require("../controllers/user-controllers");
+const {
+  validateRole,
+  validateEmail,
+  validateUserId,
+} = require("../helpers/db-validators");
+const { inputValidator } = require("../middlewares/inputValidate");
 
 const routerUser = Router();
 
+routerUser.get("/usuarios", getUser);
 
-routerUser.get("/",getUser)
+routerUser.post(
+  "/usuarios",
+  [
+    check("name", "el nombre es obligatorio").not().isEmpty(),
+    check("password", "el password es obligatorio y mas de  5 letras").isLength(
+      { min: 6 }
+    ),
+    check("email").custom((email) => validateEmail(email)),
+    check("rol").custom((rol) => validateRole(rol)),
+    inputValidator,
+  ],
+  addUser
+);
 
-routerUser.post("/",[
-    check("name","el nombre es obligatorio").not().isEmpty(),
-    check("email","el correo no tiene el formato adecuado").isEmail(),
-    check("password","el password es obligatorio y mas de  5 letras").isLength({min:6}),
-    /* check("rol","no es un rol valido").isIn(["ADMIN_ROLE", "USER_ROLE"]), */
-    check("rol").custom(async (rol="") =>{
-        const isRoleExits = await Role.findOne({rol})
-        if(!isRoleExits){
-            throw new Error(`El rol ${rol} no esta regitrado`)
-        }
-    }),
-    inputValidator
-],addUser)
+routerUser.delete(
+  "/usuarios/:id",
+  [
+    check("id", "el id no es valido").isMongoId(),
+    check("id").custom(validateUserId),
+    inputValidator,
+  ],
+  deleteUser
+);
 
-routerUser.delete("/:id",deleteUser)
-
-routerUser.put("/:id",editUser)
+routerUser.put(
+  "/usuarios/:id",
+  [
+    check("id", "el id no es valido").isMongoId(),
+    check("id").custom(validateUserId),
+    check("rol").custom((rol) => validateRole(rol)),
+    inputValidator,
+  ],
+  editUser
+);
 
 module.exports = routerUser;
